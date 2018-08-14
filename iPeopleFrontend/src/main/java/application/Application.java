e05d24e63f52a681a7eccad28e4fb493ebccddf2
MBx85 <brand-markus@web.de>
2018-08-14 07:21:20 +0200

    1st real commit

--- iPeopleFrontend/src/main/java/application/Application.java
+++ iPeopleFrontend/src/main/java/application/Application.java
@@ -2,11 +2,22 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class Application {
+	
+/*	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
 	}
 }
